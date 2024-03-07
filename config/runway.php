1eb56e2105a2d624f15d695c30fdb44ef68e0c3f
vandemonica <inircahya@gmail.com>
2024-03-07 15:38:29 +0700

    build: install runway addon

---
+++ config/runway.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Resources
+    |--------------------------------------------------------------------------
+    |
+    | Configure the resources (models) you'd like to be available in Runway.
+    |
+    */
+
+    'resources' => [
+        \App\Models\Blog::class => [
+            'name' => 'Blogs',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Disable Migrations?
+    |--------------------------------------------------------------------------
+    |
+    | Should Runway's migrations be disabled?
+    | (eg. not automatically run when you next vendor:publish)
+    |
+    */
+
+    'disable_migrations' => false,
+
+];
